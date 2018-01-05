5008c6e59f89a89c155c506988d7b15622747cf7
shauntc <shaun@shauntc.com>
2018-01-05 15:49:57 -0800

    Remove pods folder

--- Pods/Target Support Files/Pods-Iron/Pods-Iron-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Iron : NSObject
-@end
-@implementation PodsDummy_Pods_Iron
-@end
--- Pods/Target Support Files/Pods-Iron/Pods-Iron-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_IronVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_IronVersionString[];
-
